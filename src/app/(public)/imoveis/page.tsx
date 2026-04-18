2539f19c2174a98d06a1c5c5cbcba598636db9e8
fabinho2215 <fabiodenis123.fd@gmail.com>
2026-04-18 01:51:10 -0300

    .

--- src/app/(public)/imoveis/page.tsx
+++ src/app/(public)/imoveis/page.tsx
@@ -1,27 +1,33 @@
 
-export default function Home() {
+// TAGS DE CARACTERÍSTICAS DOS IMÓVEIS NOS CARDS
+import { PlaceHolderImages } from "@/lib/placeholder-images";
+import Image from "next/image";
+import { properties, Property } from "@/lib/data";
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
+import { Badge } from "@/components/ui/badge";
+import { Bed, Bath, Maximize, MapPin } from "lucide-react";
+import { PropertyCard } from "@/components/PropertyCard";
+import { Metadata } from "next";
+import { properties as mockProperties } from "@/app/(public)/imoveis/lib/data"; 
+
+
+
+
+
+
+export default function ImoveisPage() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <div className="container py-10">    
+      <h1 className="text-3xl font-bold mb-8">Imóveis para alugar e vender</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        {properties.map(property => (
+          <PropertyCard key={property.id} property={property} />
+        ))}
+      </div>
     </div>
+
+                    
+    
   );
 }
+    
